96ed8e52bd967df06adf831126a5d94b61abe28b
José Miguel Barros da Silva <miguelbarros1983@hotmail.com>
2018-03-14 18:55:25 +0000

    Create two more view(Thanks and LisResponses) + related Control methods

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -2,21 +2,35 @@
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
 
-        public ViewResult RespForm()
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View("RspvForm");
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
 
         public IActionResult About()
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PartyInvites.Models
+{
+    using System.Collections.Generic;
+
+    public static class Repository
+    {
+        private static readonly List<GuestResponse> _responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses => _responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            _responses.Add(response);
+        }
+    }
+}
